88c248829552b3fc259fc7e44256ba8fd3515a44
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-08-30 16:07:37 +0100

    update path

--- src/modules/reports.js
+++ src/modules/reports.js
@@ -1,14 +1,15 @@
 import config from "@/modules/config.js";
 import axios from "axios";
 
-const {getDate, getMetadata} = config;
+const {getMetadata} = config;
 
 const reports = {
     getMetadata: (reportName) => {
         return getMetadata(reportName);
     },
     getAllRecords: async (reportName) => {
-        const reportUrl = `/reports/${reportName}.json`;
+        const hostPath = `${window.location.host}/version-reporter-dashboard`;
+        const reportUrl = `${hostPath}/reports/${reportName}.json`;
         try {
             return axios.get(reportUrl, {responseType: 'json'});
             // return response.data;
