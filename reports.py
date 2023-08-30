620dca734589a2e0e67d3180d622ee97e518dc84
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-08-30 18:46:03 +0100

    Adding cron job to fetch data

---
+++ reports.py
@@ -0,0 +1,48 @@
+import os
+import json
+from azure.cosmos import CosmosClient, exceptions
+
+# Get secrets from Git, exit if any is missing
+try:
+    COSMOS_KEY = os.environ["COSMOS_KEY"]
+    COSMOS_DB_NAME = os.environ["COSMOS_DB_NAME"]
+    COSMOS_DB_URI = os.environ["COSMOS_DB_URI"]
+except KeyError as k:
+    print(f"Missing key. Error", k, "Job will exit and not complete")
+    raise
+
+# Add new containers to the list
+containers = [
+    "renovate",
+    "paloalto",
+    "helmcharts"
+]
+
+try:
+    # Connect to cosmos
+    client = CosmosClient(COSMOS_DB_URI, COSMOS_KEY)
+    database = client.get_database_client(COSMOS_DB_NAME)
+
+    # Read all records from container
+    for container in containers:
+        reports = []
+        db_container = database.get_container_client(container)
+
+        for item in db_container.query_items(
+                query='SELECT * FROM c',
+                enable_cross_partition_query=True):
+            reports.append(item)
+
+        # Save json data to the reports folder under public folder
+        file_name = f"./public/reports/{container}.json"
+        with open(file_name, 'w') as report_file:
+            json.dump(reports, report_file, indent=4)
+
+        print(f"Done writing JSON data into file: {file_name}")
+
+except exceptions.CosmosHttpResponseError as http_response_error:
+    print(f"Saving to file from db failed with CosmosHttpResponseError error: {http_response_error}")
+    raise
+
+# Done? let us know
+print(f"Save to report files completed for {containers}.")
