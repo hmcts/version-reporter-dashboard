aefc531f9530ff1d111e7dd61f8f3557cd5fa3a8
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-08-30 16:49:26 +0100

    update

--- src/modules/reports.js
+++ src/modules/reports.js
@@ -8,7 +8,7 @@ const reports = {
         return getMetadata(reportName);
     },
     getAllRecords: async (reportName) => {
-        const reportUrl = `${window.location.host}/reports/${reportName}.json`;
+        const reportUrl = `https://hmcts.github.io/version-reporter-dashboard/reports/${reportName}.json`;
         try {
             return axios.get(reportUrl, {responseType: 'json'});
             // return response.data;
